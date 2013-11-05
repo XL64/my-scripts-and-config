3a462506a7c1a9a9b444ce70114c8b8adace21d4
Xavier Lacoste <xl64100@gmail.com>
2013-11-05 10:39:21 +0100

    add script to update all scotch

---
+++ mygale_install_tools/install_all_scotch.sh
@@ -0,0 +1,8 @@
+python missing_installs.py -p scotch-nompi -v 6.0.0 -lc -d -i ~/src/scotch/scotch_6.0.0
+python missing_installs.py -p scotch-nompi -v 5.1.12b -lc -d -i ~/src/scotch/scotch_5.1.12b
+python missing_installs.py -p scotch-nompi-esmumps -v 6.0.0 -lc -d -i ~/src/scotch/scotch_6.0.0_esmumps
+python missing_installs.py -p scotch-nompi-esmumps -v 5.1.12b -lc -d -i ~/src/scotch/scotch_5.1.12_esmumps
+python missing_installs.py -p scotch -v 6.0.0 -lc -d -i ~/src/scotch/scotch_6.0.0
+python missing_installs.py -p scotch -v 5.1.12b -lc -d -i ~/src/scotch/scotch_5.1.12b
+python missing_installs.py -p scotch-esmumps -v 6.0.0 -lc -d -i ~/src/scotch/scotch_6.0.0_esmumps
+python missing_installs.py -p scotch-esmumps -v 5.1.12b -lc -d -i ~/src/scotch/scotch_5.1.12_esmumps
